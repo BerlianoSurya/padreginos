11526cd5f2f173eba267854413f70a6574eb1279
BerlianoSurya <berlianosurya@gmail.com>
2025-03-27 16:21:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+import { createRoot } from "react-dom/client";
+// import Pizza from "./Pizza";
+import PizzaOfTheDay from "./PizzaOfTheDay";
+import Order from "./Order";
+import Header from "./Header";
+import { CartContext } from "./contexts";
+import { useState } from "react";
+
+const App = () => {
+  const cartHook = useState([]);
+  return (
+    <CartContext.Provider value={cartHook}>
+      <div>
+        <Header />
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </CartContext.Provider>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
